ccbec8008c17cc457a592345793a010c6dc3d5b6
oculosdanilo <oculosdanilo@gmail.com>
2024-03-04 18:51:43 -0300

    componentes futuros

---
+++ src/app/home/wiki/wiki.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wiki',
+  standalone: true,
+  imports: [],
+  templateUrl: './wiki.component.html',
+  styleUrl: './wiki.component.scss'
+})
+export class WikiComponent {
+
+}
