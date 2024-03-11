9b98970736624e474aa38bbd3bedc23c80c88ada
oculosdanilo <oculosdanilo@gmail.com>
2024-03-10 21:16:37 -0300

    eu odeio canvas eu odeio html eu odeio jacascript eu simplesmente odeio programação web por completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import * as $ from 'jquery';
-import { Component } from '@angular/core';
-import { TemaService } from './tema.service';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {TemaService} from './tema.service';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'gatoroot',
@@ -11,13 +11,16 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
 })
 export class AppComponent {
-  constructor(private servicoTema: TemaService) {}
+  constructor(private servicoTema: TemaService) {
+  }
+
   title = 'gatopediaweb';
 
   ngOnInit() {
     if (window.matchMedia('(max-width: 769px)').matches) {
       const mensagem: string =
         'Oops! Aparentemente você está usando um celular para acessar o site, que não tem otimização para telas menores. Para telefones Android, instale o app pelo Github.\nPor motivos técnicos, não é possível utilizar o aplicativo em celulares com iOS.';
+      window.alert(mensagem);
       window.alert(mensagem);
     }
     this.servicoTema.chequeInicial();
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,13 @@ import {cookies} from '../cookies.service';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {ModoTema, TemaService} from '../tema.service';
-import {NgIf} from '@angular/common';
+import {NgIf, NgOptimizedImage} from '@angular/common';
 import {animate, style, transition, trigger} from '@angular/animations';
 import {MatDividerModule} from '@angular/material/divider';
 import {NzIconModule} from 'ng-zorro-antd/icon';
 import {FirebaseServiceDatabase, User} from '../firebasedb.service';
 import {FirebaseServiceStorage} from '../firebasest.service';
+import Cropper from 'cropperjs';
 
 const popupAnimation = trigger('aparecer', [
   transition(':enter', [
@@ -38,7 +39,7 @@ export var userInfo: User;
   styleUrls: ['./home.component.scss'],
   standalone: true,
   animations: [popupAnimation],
-  imports: [MatButtonModule, MatIconModule, NgIf, MatDividerModule, NzIconModule],
+  imports: [MatButtonModule, MatIconModule, NgIf, MatDividerModule, NzIconModule, NgOptimizedImage, MatButtonModule],
 })
 export class HomeComponent {
   username = '';
@@ -46,38 +47,61 @@ export class HomeComponent {
   tooltipSair = document.getElementById('tooltip')!;
   bgBlur = document.getElementById('bgBlur')!;
   ano = new Date().getFullYear();
+  jaPegouFoto_profilePic: boolean = false;
+  photoEdit = document.getElementById('photoEdit')!;
+
+  src = 'assets/user.webp';
 
   popupSair = false;
   popupConfig = false;
   popupProfile = false;
+  popupProfileEdit = false;
 
-  constructor(private cookies: cookies, private tema: TemaService, private firebaseDB: FirebaseServiceDatabase, private firebaseSt: FirebaseServiceStorage) {
+  constructor(private cookies: cookies, private tema: TemaService, private firebaseDB: FirebaseServiceDatabase,
+              private firebaseSt: FirebaseServiceStorage) {
   }
 
   ngOnInit() {
     this.username = atob(this.pegarUsername());
+    this.username = atob(this.pegarUsername());
 
     this.init().then();
-
-    $(document).on('click', (e) => {
-      console.log(e.target);
-    });
   }
 
   async init() {
-    const userInfoDB = await this.firebaseDB.getUser(this.username);
-    if (userInfoDB)
-      userInfo = userInfoDB;
+    userInfo = (await this.firebaseDB.getUser(this.username))!;
 
-    if (userInfo.img) {
-      const imagemURL = await this.firebaseSt.pegarFotoDoUsuario(this.username);
+    if (userInfo.img)
+      this.src = await this.firebaseSt.pegarFotoDoUsuario(this.username);
+  }
 
-      $('#profile').css('background-image', `url(${imagemURL})`);
-    }
+  quandoPegarFoto() {
+    const profileSelect = document.getElementById('profileSelect') as HTMLInputElement;
+    const file = profileSelect.files![0];
+    const fr = new FileReader();
+
+    fr.onload = () => {
+      let crop = document.getElementById('crop') as HTMLImageElement;
+
+      crop.onload = () => {
+        let c = new Cropper(crop, {
+          aspectRatio: 1,
+        });
+
+        $('#salvarCrop').on('click', () => {
+          let canv = c.getCroppedCanvas();
+          console.log(canv.toDataURL());
+        });
+      };
+
+      crop.src = fr.result as string;
+    };
+
+    fr.readAsDataURL(file);
   }
 
   pegarUsername(): string {
-    if (this.cookies.get('u') != null) {
+    if (this.cookies.get('u')) {
       return this.cookies.get('u')!;
     } else {
       return sessionStorage.getItem('u')!;
@@ -113,11 +137,13 @@ export class HomeComponent {
   }
 
   sair() {
-    if (this.cookies.get('u') != null) {
+    if (this.cookies.get('u')) {
       this.cookies.delete('u');
     } else {
       sessionStorage.removeItem('u');
     }
     location.reload();
   }
+
+  protected readonly console = module;
 }
